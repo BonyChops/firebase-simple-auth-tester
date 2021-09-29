e7564226a147b43a4e61b56ebea747952157c5a3
BonyChops <contact.bonychops@gmail.com>
2021-09-29 18:17:46 +0900

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import app from "./firebase";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useState } from 'react';
 
 function App() {
+  const [token, setToken] = useState(false)
+  const auth = () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        auth.currentUser.getIdToken(true).then(function (idToken) {
+          setToken(idToken);
+        }).catch(function (error) {
+          // Handle error
+        });
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={auth} >Login</button>
+      <p>{token === false ? "Not Logged in." : `Your token: ${token}`}</p>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,6 @@
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from "./key";
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
